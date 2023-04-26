299546afd339aa454200ca1c0651bda6e8f73284
chrismdating1206@gmail.com <christian.dating12>
2023-04-26 22:20:24 +0800

    intro

--- Program-1.py
+++ Program-1.py
@@ -4,3 +4,11 @@
 # will create two other text file; the first text file will be named even.txt that will contains all even numbers 
 # extracted from the numbers.txt. The second file will be named odd.txt that will contains all odd numbers 
 # extracted from the numbers.txt.
+
+input("\n\033[94mPress Any Key to Start...")
+print("\033[90m=" * 80)
+
+# Intro
+import pyfiglet
+greet = "GOOD DAY!"
+print("\033[92m" + pyfiglet.figlet_format(greet, font = "Thin"))
