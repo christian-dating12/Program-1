51d91633974eb59f424b49ff681f3af6f2e84387
chrismdating1206@gmail.com <christian.dating12>
2023-04-26 22:17:16 +0800

    first commit

---
+++ Program-1.py
@@ -0,0 +1,6 @@
+# Chistian Dating | BSCPE 1-5
+
+# Write a python program that reads a text file named numbers.txt that contains 20 integers. The program
+# will create two other text file; the first text file will be named even.txt that will contains all even numbers 
+# extracted from the numbers.txt. The second file will be named odd.txt that will contains all odd numbers 
+# extracted from the numbers.txt.
